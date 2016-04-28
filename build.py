1839b7e785701d8f9f6a392dba0a0de88296c91b
Ben Martin <ben@benmartinstudios.com.au>
2016-04-28 15:38:08 +1000

    Added convenience method to clear test; Tweaked build_test signature.

--- build.py
+++ build.py
@@ -3,19 +3,16 @@
 import os
 import argparse
 
-def build_test(test):
+def build_test(test_runner, executable, options):
     """
     Builds a standalone test executable based upon test configuration.
 
-    :param test: A dictionary containing the test's configuration. Has keys:
-        test_runner: The test runner to use (i.e. suite.py)
-        executable: The name of the output file (i.e. assign1_sample_tests.py)
-        options: A dictionary whose keys override the given defaults in test_runner.
-    """
+    :param test_runner: The test runner to use (i.e. suite.py)
+    :param executable: The name of the output file (i.e. assign1_sample_tests.py)
+    :param options: A dictionary whose keys override the given defaults in test_runner.
 
-    test_runner = test['test_runner']
-    executable = test['executable']
-    options = test['options']
+    :return: None
+    """
 
     if options['TEST_DATA_RAW']:
         with open(options['TEST_DATA_RAW'], 'r') as fd:
@@ -44,12 +41,31 @@ def build_test(test):
     with open(executable, 'w') as fd:
         fd.writelines(output)
 
+def clear_test(executable):
+    """
+    Deletes the executable for a given test.
+
+    :param executable: The executable to delete.
+
+    :return: None
+    """
+    try:
+        os.remove(executable)
+    except OSError:
+        pass
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("manifest",
                         help="The build manifest.",
                         nargs="?",
                         default="manifest.py")
+    parser.add_argument("-r", "--remove",
+                        help="Removes the executables.",
+                        action="store",
+                        type=bool,
+                        default=False)
 
     args = parser.parse_args()
 
@@ -59,6 +75,9 @@ if __name__ == "__main__":
     os.chdir('./' + os.path.dirname(args.manifest))
 
     for test in manifest.tests:
-        build_test(test)
+        if args.remove:
+            clear_test(test['executable'])
+        else:
+            build_test(**test)
 
 
